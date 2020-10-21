ac1c68a24b9209fbbfc1695137a543d7be57d4a1
ShaqEd92 <shaqhaedwards@gmail.com>
2020-10-21 02:31:46 -0400

    svg files

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ScissorsImage from "./components/images/ScissorsImage";
+import PaperImage from "./components/images/PaperImage";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="container">
-      <ScissorsImage />
+      {/* <ScissorsImage /> */}
+      <PaperImage/>
     </div>
   );
 }
