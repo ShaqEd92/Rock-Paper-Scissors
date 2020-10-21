41c5bd1b92561372db16511ffbc28f81611302a7
ShaqEd92 <shaqhaedwards@gmail.com>
2020-10-20 23:52:32 -0400

    jk using CRA

---
+++ rock-paper-scissors/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/App.css";
+import scissors from "./images/scissors.svg";
+
+function App() {
+  return (
+    <div className="container">
+      <img src={scissors} alt="scissors" />
+    </div>
+  );
+}
+
+export default App;
