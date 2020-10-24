5c8f17e86f86753940fe563b2e09b32268bf46a5
ShaqEd92 <shaqhaedwards@gmail.com>
2020-10-24 03:11:33 -0400

    homepage

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import GameOptions from "./components/GameOptions";
+import PlayGame from "./components/PlayGame";
+import GameOver from "./components/GameOver";
 import ScissorsImage from "./components/images/ScissorsImage";
 import PaperImage from "./components/images/PaperImage";
 import "./styles/App.css";
 
 function App() {
+  const [player, setPlayer] = useState({ name: "", score: 0 });
+  const [rounds, setRounds] = useState();
+  const [gameResult, setGameResult] = useState("");
+
   return (
-    <div className="container">
-      {/* <ScissorsImage /> */}
-      <PaperImage/>
-    </div>
+    <Switch>
+      <div className="container">
+        <header>
+          <h1>ROCK&nbsp;&nbsp;PAPER&nbsp;&nbsp;SCISSORS</h1>
+        </header>
+        <main>
+          <Route exact path="/">
+            <Link className="start-button" to="/start">
+              START GAME
+            </Link>
+          </Route>
+          <Route exact path="/start">
+            <GameOptions setPlayer={setPlayer} setRounds={setRounds} />
+          </Route>
+          <Route exact path="/play">
+            <PlayGame
+              player={player}
+              setRounds={rounds}
+              setGameResult={setGameResult}
+            />
+          </Route>
+          <Route exact path="/start">
+            <GameOver gameResult={gameResult} />
+          </Route>
+        </main>
+      </div>
+    </Switch>
   );
 }
 
--- rock-paper-scissors/src/components/GameOver.js
+++ rock-paper-scissors/src/components/GameOver.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Header = () => {
+const GameOver = () => {
   return <div></div>;
 };
 
-export default Header;
+export default GameOver;
